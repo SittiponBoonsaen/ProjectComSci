f5655e7c1828d43944e26b0ed83d54c7edc00acc
fastfivesadd <sittipon46160@gmail.com>
2020-10-01 09:34:22 +0700

    commit 1/10
    commit 1/10

--- myApp/src/app/tabs/myhome/myhome.module.ts
+++ myApp/src/app/tabs/myhome/myhome.module.ts
@@ -7,13 +7,25 @@ import { IonicModule } from '@ionic/angular';
 import { MyhomePageRoutingModule } from './myhome-routing.module';
 
 import { MyhomePage } from './myhome.page';
+import {TableModule} from 'primeng/table';
+import {ButtonModule} from 'primeng/button';
+import {ProgressSpinnerModule} from 'primeng/progressspinner';
+import {DataViewModule} from 'primeng/dataview';
+import {DropdownModule} from 'primeng/dropdown';
+import {RatingModule} from 'primeng/rating';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    MyhomePageRoutingModule
+    MyhomePageRoutingModule,
+    TableModule,
+    ButtonModule,
+    ProgressSpinnerModule,
+    DataViewModule,
+    DropdownModule,
+    RatingModule
   ],
   declarations: [MyhomePage]
 })
