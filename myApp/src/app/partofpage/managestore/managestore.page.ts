0674bf3d0fb695075222d13bffb30741d48a8f8b
fastfivesadd <sittipon46160@gmail.com>
2020-10-16 09:38:43 +0700

    16/10/63 part1

---
+++ myApp/src/app/partofpage/managestore/managestore.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-managestore',
+  templateUrl: './managestore.page.html',
+  styleUrls: ['./managestore.page.scss'],
+})
+export class ManagestorePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
